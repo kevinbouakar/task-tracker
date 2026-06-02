2013a0a65937d51749b03db21eec023a237afb33
Kevin Bou Akar <bouakarkevin@gmail.com>
2026-06-02 18:49:07 +0300

    Added proper task list management and log out functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
- Mock authentication checking for hardcoded credentials and displaying dashboard upon valid input.
- */
+    1. Mock authentication checking for hardcoded credentials and displaying dashboard upon valid input.
+*/
 const form = document.querySelector("#login-form");
 
 //! Check if loggedIn available before showing login form
@@ -25,3 +25,41 @@ form.addEventListener("submit", function(event){
         document.querySelector("#error-message").classList.remove("hidden");
     }
 });
+
+/**
+    2. Task List Management (CRUD) to manage tasks.
+*/
+
+const taskForm = document.querySelector("#task-form");
+
+taskForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    //! Get task value, and append a button next to it.
+    let addedTask = document.querySelector("#task-input").value;
+    const li = document.createElement("li");
+    li.innerHTML = addedTask + "<button class='delete-task'>Delete</button>";
+
+    //! Appened the task to the task list.
+    document.querySelector("#task-list").appendChild(li);
+    document.querySelector("#task-input").value = "";
+    
+    //! Delete added task
+    deleteButton = li.querySelector(".delete-task");
+    deleteButton.addEventListener("click", function(){
+        li.remove();
+    })
+
+});
+
+/*
+    3. Proper logout and elements manipulation
+*/
+
+//! Get logout button
+const logoutButton = document.querySelector("#logout-btn");
+logoutButton.addEventListener("click", function(){
+    localStorage.removeItem("isLoggedIn");
+    document.querySelector("#login-container").classList.remove("hidden");
+    document.querySelector("#dashboard-container").classList.add("hidden");
+})
+
